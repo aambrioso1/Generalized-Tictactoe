f9a3f11e3666e6eefae651d3f070f20d187ba5ef
aambrioso1 <44902917+aambrioso1@users.noreply.github.com>
2019-05-04 10:33:09 -0400

    Update Array_Conventions.py

--- Array_Conventions.py
+++ Array_Conventions.py
@@ -1,11 +1,11 @@
-# We begin with the follow structure.
+# We begin with the following structure.
 # The board is a numpy array of integers.
 # An empty square is represent by a 0.
 # The first player’s moves are  represented by 1.
 # The second player's moves are represented by -1.
 # We adopt the convention that the squares are represented by a tuple of integers (row, col).
-# Also useful is the convention that the squares are numbered from 1 to m x n starting at the
-# upper left and move across a row then to the next row.
+# We will also use the convention that the squares are numbered from 1 to (number of rows) x (number of columns) starting at the
+# upper left and moving across a row then to the next row.
 
 from numpy import zeros
 
