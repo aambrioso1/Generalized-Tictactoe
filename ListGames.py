10ae2be9f3bb1dd19ed642f3cf0621fc8b21ad38
Alex Ambrioso <aambrioso1@gmail.com>
2019-05-06 10:24:28 -0400

    Update ListGames.py

--- ListGames.py
+++ ListGames.py
@@ -1,2 +1,6 @@
-# Create a function ListGames(m,n) that generates a file with all possible "games."   A "game" is a board completely filled with x's and o's such that the number of x's is either equal to the number o's or greater by 1 depending on whether there are an even or odd number of squares.   This functions will be useful for finding won and drawn games.   We can use the list for later attempts at machine learning.
+# Create a function ListGames(r,c) that generates a file with all possible "games."   A "game" is a board completely filled with x's and o's such that the number of x's is either equal to the number o's or greater by 1 depending on whether there are an even or odd number of squares.   This functions will be useful for finding won and drawn games.   We can use the list for later attempts at machine learning.
+
+# This will result in 2^(m x n) "games."  That should be small enough assuming r x c is <= 25.
+
+# This is not really the number of games since the order of play makes and difference in a game.   Also a game ends when either player gets n in a row.
 
