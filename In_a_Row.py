fbf50556d45536aff4031f80ac81bded2706ecca
Alex Ambrioso <aambrioso1@gmail.com>
2019-05-06 10:01:07 -0400

    Create In_a_Row.py

---
+++ In_a_Row.py
@@ -0,0 +1,7 @@
+#  This function matrix, square (sq), and integer or string as an argument and decide how many of the object in a row there are connected #  to the square.
+
+def In_a_Row(matrix, sq, obj):
+  #code
+ 
+ #the non-negative integer n is the number of times obj occurs in-a-row and connected to the square sq if sq does not contain the obj, 0 is returned.
+ return n
